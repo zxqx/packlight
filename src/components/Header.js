477e4730999b5e0416486685ba9ac268aa735c38
Zak Angelle <zaknotzak@gmail.com>
2016-06-17 18:17:42 -0500

    Moved user info stuff to separate component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,18 @@
 import React, { Component, PropTypes } from 'react';
-import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
 import { Link } from 'react-router';
 import cssModules from 'react-css-modules';
+import UserInfo from './UserInfo';
 import styles from '../style/header.scss';
-import 'react-simple-dropdown/styles/Dropdown.css';
 
 @cssModules(styles)
 export default class Header extends Component {
   static propTypes = {
     logoutUser: PropTypes.func.isRequired,
-    dropdownStyles: PropTypes.object,
     styles: PropTypes.object
   };
 
-  logoutUser() {
-    this.props.logoutUser();
-  }
-
   render() {
-    const { styles, user } = this.props;
+    const { styles, user, logoutUser } = this.props;
 
     return (
       <div className={styles.navbar}>
@@ -30,22 +24,8 @@ export default class Header extends Component {
           </div>
 
           {user.isAuthenticated ?
-            <Dropdown className={styles.user}>
-              <DropdownTrigger>
-                <img className={styles.avatar} src={user.avatar} alt={user.email} />
-                {user.email}
-                <span className={styles.caret}>&#9660;</span>
-              </DropdownTrigger>
-              <DropdownContent styleName='dropdown-content'>
-                <ul>
-                  <li>
-                    <a onClick={this.logoutUser.bind(this)}>Log Out</a>
-                  </li>
-                </ul>
-              </DropdownContent>
-            </Dropdown>
-            :
-            <Link to="/login" styleName='login-btn'>Log in</Link>
+            <UserInfo user={user} logoutUser={logoutUser} />
+            : <Link to="/login" styleName="login-btn">Log in</Link>
           }
         </div>
       </div>
---
+++ src/components/UserInfo.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
+import cssModules from 'react-css-modules';
+import styles from '../style/header.scss';
+import 'react-simple-dropdown/styles/Dropdown.css';
+
+@cssModules(styles)
+export default class Header extends Component {
+  static propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    styles: PropTypes.object
+  };
+
+  logoutUser() {
+    this.props.logoutUser();
+  }
+
+  render() {
+    const { styles, user } = this.props;
+
+    return (
+      <Dropdown className={styles.user}>
+        <DropdownTrigger>
+          <img className={styles.avatar} src={user.avatar} alt={user.email} />
+          {user.email}
+          <span className={styles.caret}>&#9660;</span>
+        </DropdownTrigger>
+
+        <DropdownContent styleName="dropdown-content">
+          <ul>
+            <li>
+              <a onClick={this.logoutUser.bind(this)}>Log Out</a>
+            </li>
+          </ul>
+        </DropdownContent>
+      </Dropdown>
+    );
+  }
+}
